68ca867e8a0d85da22d64dbc9d42f0360e878b1a
Micah Herrera <micah@micahherrera.com>
2016-07-11 16:15:43 -0500

    stop on main

--- Hotify/app/src/main/java/com/example/hotify/MainActivity.java
+++ Hotify/app/src/main/java/com/example/hotify/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.hotify;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button stopButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        stopButton = (Button)findViewById(R.id.stop);
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent stopServiceIntent = new Intent(MainActivity.this, SongService.class);
+                stopService(stopServiceIntent);
+                Log.d("SONG", "onClick: stop pressed");
+            }
+        });
+
+
     }
 }
